b5e6ddc98cd7d9c8d0c9d72656939dc5ecf07138
bala-amypo <bala@amypo.in>
2025-12-18 08:53:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/IntegrityCase.java
+++ src/main/java/com/example/demo/entity/IntegrityCase.java
@@ -1,98 +1,14 @@
-package com.example.demoentity;
+package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 
 public class IntegrityCase {
-
+    @Id
     private Long id;
-    private String caseNumber;
-    private String title;
+    private String CourseCode;
+    private String InstructName;
     private String description;
-    private String severity;
     private String status;
-    private LocalDateTime createdDate;
-    private LocalDateTime resolvedDate;
-
-    public IntegrityCase() {
-        this.createdDate = LocalDateTime.now();
-        this.status = "OPEN";
-    }
-
-    public IntegrityCase(Long id, String caseNumber, String title, String severity) {
-        this.id = id;
-        this.caseNumber = caseNumber;
-        this.title = title;
-        this.severity = severity;
-        this.status = "OPEN";
-        this.createdDate = LocalDateTime.now();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCaseNumber() {
-        return caseNumber;
-    }
-
-    public void setCaseNumber(String caseNumber) {
-        this.caseNumber = caseNumber;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getSeverity() {
-        return severity;
-    }
-
-    public void setSeverity(String severity) {
-        this.severity = severity;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public LocalDateTime getResolvedDate() {
-        return resolvedDate;
-    }
-
-    public void setResolvedDate(LocalDateTime resolvedDate) {
-        this.resolvedDate = resolvedDate;
-    }
-
-    @Override
-    public String toString() {
-        return "IntegrityCase{" +"id=" + id +", caseNumber='" + caseNumber + '\'' +", title='" + title + '\'' +", severity='" + severity + '\'' +", status='" + status + '\'' +'}';
-    }
+    private LocalDate incidentdate;
+    private LocalDateTime createdAt;
 }
