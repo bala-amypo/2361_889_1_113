752940bc8f427495106bceded8a0a8775893bfe4
bala-amypo <bala@amypo.in>
2025-12-18 08:39:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentProfile.java
+++ src/main/java/com/example/demo/entity/StudentProfile.java
@@ -1,7 +1,7 @@
 package com.example.demo.entity;
 
 public class StudentProfile{
-    @Id;
+    @Id
     private Long id;
     private String studentId;
     private String name;
@@ -10,4 +10,71 @@ public class StudentProfile{
     private int yearLevel;
     private boolean isRepeatOffender;
     private LocalDateTime createdAt;
+
+    public Long getId() {A
+    return id;
+}
+public StudentProfile() {
+}
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+public String getStudentId() {
+    return studentId;
+}
+
+public void setStudentId(String studentId) {
+    this.studentId = studentId;
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public String getEmail() {
+    return email;
+}
+
+public void setEmail(String email) {
+    this.email = email;
+}
+
+public String getProgram() {
+    return program;
+}
+
+public void setProgram(String program) {
+    this.program = program;
+}
+
+public int getYearLevel() {
+    return yearLevel;
+}
+
+public void setYearLevel(int yearLevel) {
+    this.yearLevel = yearLevel;
+}
+
+public boolean isRepeatOffender() {
+    return isRepeatOffender;
+}
+
+public void setRepeatOffender(boolean repeatOffender) {
+    isRepeatOffender = repeatOffender;
+}
+
+public LocalDateTime getCreatedAt() {
+    return createdAt;
+}
+
+public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+}
+
 }
