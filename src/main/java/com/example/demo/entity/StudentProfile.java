4df36085dc25c535a54e5b240b592ca8d4732da5
bala-amypo <bala@amypo.in>
2025-12-18 08:34:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentProfile.java
+++ src/main/java/com/example/demo/entity/StudentProfile.java
@@ -1,7 +1,13 @@
 package com.example.demo.entity;
 
 public class StudentProfile{
+    @Id;
     private Long id;
+    private String studentId;
     private String name;
-    private Stringz
+    private String email;
+    private String program;
+    private int yearLevel;
+    private boolean isRepeatOffender;
+    private LocalDateTime createdAt;
 }
