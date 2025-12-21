9b729ae896b7b75e4d5093f59e03ca3e4c993b33
bala-amypo <bala@amypo.in>
2025-12-21 16:04:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/EvidenceRecordService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.EvidenceRecord;
+
+public interface EvidenceRecordService {
+    EvidenceRecord submitEvidence(EvidenceRecord evidenceRecord);
+}
