4f72c9154a95134bd15d233d9d3c96bedbfd7692
bala-amypo <bala@amypo.in>
2025-12-26 06:08:39 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -26,4 +26,7 @@ public class RegisterRequest {
 
     public String getRoleName() { return roleName; }
     public void setRoleName(String roleName) { this.roleName = roleName; }
+    
+    public String getRole() { return roleName; }
+    public void setRole(String role) { this.roleName = role; }
 }
