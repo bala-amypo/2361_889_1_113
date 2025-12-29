834d6dd43b25041ba4008ce6460dbdb217a916be
bala-amypo <bala@amypo.in>
2025-12-29 08:56:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/AppUser.java
+++ src/main/java/com/example/demo/entity/AppUser.java
@@ -46,7 +46,6 @@ public class AppUser {
         this.password = password;
     }
 
-    // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
