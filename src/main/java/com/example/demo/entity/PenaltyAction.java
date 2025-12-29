b7cdf5cb261f926b321d70c53395299de52af04a
bala-amypo <bala@amypo.in>
2025-12-29 08:59:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/PenaltyAction.java
+++ src/main/java/com/example/demo/entity/PenaltyAction.java
@@ -38,7 +38,6 @@ public class PenaltyAction {
         this.issuedBy = issuedBy;
     }
 
-    // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
